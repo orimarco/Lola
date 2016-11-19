c81cb71bc0b9ca19bf1de1af4bc8bf4b107a5a1a
orimarco <marcovitch.ori@gmail.com>
2016-11-19 21:29:38 +0200

    spartanize a little more

--- src/main/java/il/ac/technion/cs/ssdl/lola/parser/re/Branch.java
+++ src/main/java/il/ac/technion/cs/ssdl/lola/parser/re/Branch.java
@@ -5,79 +5,81 @@ import java.util.*;
 import il.ac.technion.cs.ssdl.lola.parser.*;
 
 public class Branch implements Cloneable {
-  public List<RegExp> res;
-  private int idx;
+	public List<RegExp> res;
+	private int idx;
 
-  Branch(final List<RegExp> res, final int idx) {
-    this.res = res;
-    this.idx = idx;
-  }
-  public void apply(final PythonAdapter a) {
-    for (final RegExp re : res)
-      re.apply(a);
-  }
-  @Override public Branch clone() {
-    final List<RegExp> newRes = new ArrayList<>();
-    for (final RegExp re : res)
-      newRes.add(re.clone());
-    return new Branch(newRes, idx);
-  }
-  public boolean diverges(final Bunny b) {
-    int divergents = 0;
-    int i = idx;
-    while (i < res.size()) {
-      if (res.get(i).eats(b))
-        ++divergents;
-      if (!res.get(i).satiated())
-        break;
-      ++i;
-    }
-    return divergents > 1;
-  }
-  public boolean eats(final Bunny b) {
-    int i = idx;
-    while (i < res.size()) {
-      if (res.get(i).eats(b))
-        return true;
-      if (!res.get(i).satiated())
-        return false;
-      ++i;
-    }
-    return false;
-  }
-  /** Assuming eats(b) */
-  public void feed(final Bunny b) {
-    while (idx < res.size()) {
-      if (res.get(idx).eats(b)) {
-        res.get(idx).feed(b);
-        return;
-      }
-      ++idx;
-    }
-    res.get(res.get(idx).eats(b) ? idx : ++idx).feed(b);
-  }
-  public List<Branch> feedAndGetDivergents(final Bunny b) {
-    assert eats(b);
-    final ArrayList<Branch> $ = new ArrayList<>();
-    while (idx < res.size()) {
-      if (res.get(idx).eats(b)) {
-        final Branch newBranch = clone();
-        newBranch.feed(b);
-        $.add(newBranch);
-      }
-      if (!res.get(idx).satiated())
-        break;
-      ++idx;
-    }
-    return $;
-  }
-  public boolean satiated() {
-    return idx + 1 == res.size() && res.get(idx).satiated();
-  }
-  public String text() {
-    String $ = "";
-    for (final RegExp re : res)
-      $ += re.text();
-    return $;
-  }
+	Branch(final List<RegExp> res, final int idx) {
+		this.res = res;
+		this.idx = idx;
+	}
+
+	public void apply(final PythonAdapter a) {
+		for (final RegExp ¢ : res)
+			¢.apply(a);
+	}
+
+	@Override
+	public Branch clone() {
+		final List<RegExp> newRes = new ArrayList<>();
+		for (final RegExp ¢ : res)
+			newRes.add(¢.clone());
+		return new Branch(newRes, idx);
+	}
+
+	public boolean diverges(final Bunny b) {
+		int divergents = 0;
+		for (int ¢ = idx; ¢ < res.size(); ++¢) {
+			if (res.get(¢).eats(b))
+				++divergents;
+			if (!res.get(¢).satiated())
+				break;
+		}
+		return divergents > 1;
+	}
+
+	public boolean eats(final Bunny b) {
+		for (int ¢ = idx; ¢ < res.size(); ++¢) {
+			if (res.get(¢).eats(b))
+				return true;
+			if (!res.get(¢).satiated())
+				break;
+		}
+		return false;
+	}
+
+	/** Assuming eats(b) */
+	public void feed(final Bunny ¢) {
+		for (; idx < res.size(); ++idx)
+			if (res.get(idx).eats(¢)) {
+				res.get(idx).feed(¢);
+				return;
+			}
+		res.get(res.get(idx).eats(¢) ? idx : ++idx).feed(¢);
+	}
+
+	public List<Branch> feedAndGetDivergents(final Bunny b) {
+		assert eats(b);
+		final ArrayList<Branch> $ = new ArrayList<>();
+		for (; idx < res.size(); ++idx) {
+			if (res.get(idx).eats(b)) {
+				final Branch newBranch = clone();
+				newBranch.feed(b);
+				$.add(newBranch);
+			}
+			if (!res.get(idx).satiated())
+				break;
+		}
+		return $;
+	}
+
+	public boolean satiated() {
+		return idx + 1 == res.size() && res.get(idx).satiated();
+	}
+
+	public String text() {
+		String $ = "";
+		for (final RegExp ¢ : res)
+			$ += ¢.text();
+		return $;
+	}
 }

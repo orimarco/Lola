f24a68a1a56a7fd9a4c4c2ca0021ff97278292e6
orimarco <marcovitch.ori@gmail.com>
2016-11-24 14:04:15 +0200

    forgot the examples

---
+++ src/main/java/il/ac/technion/cs/ssdl/lola/utils/az.java
@@ -0,0 +1,35 @@
+package il.ac.technion.cs.ssdl.lola.utils;
+import il.ac.technion.cs.ssdl.lola.parser.builders.SnippetToken;
+import il.ac.technion.cs.ssdl.lola.parser.builders.*;
+import il.ac.technion.cs.ssdl.lola.parser.builders.TriviaToken;
+import il.ac.technion.cs.ssdl.lola.parser.builders.AST.Node;
+/**
+ * @author Ori Marcovitch
+ * @since Nov 24, 2016
+ */
+public enum az {
+	;
+	/**
+	 * @param b JD
+	 * @return
+	 */
+	public static SnippetToken snippetToken(Node b) {
+		return b == null ? null : (SnippetToken) b;
+	}
+
+	/**
+	 * @param b JD
+	 * @return
+	 */
+	public static TriviaToken triviaToken(Node b) {
+		return b == null ? null : (TriviaToken) b;
+	}
+
+	/**
+	 * @param b JD
+	 * @return
+	 */
+	public static $Find $Find(Node b) {
+		return b == null ? null : ($Find) b;
+	}
+}
